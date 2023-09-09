bd04ab4809000f595a6b856598861ffc3f8a37cb
snoopyods <snoopyods@gmail.com>
2023-09-09 10:38:13 -0700

    Login working confirmed via postman

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -21,7 +21,24 @@ router.post("/register", async (req, res) => {
     res.json({ message: "User added successfuly!" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+        return res.json({ message: "User does not exist."});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.json({ message: "Username or Password is incorrect."})
+    }
+
+    const token = jwt.sign({id: user._id}, "secretsecret");
+    res.json({ token, userID: user._id });
+
+});
 
 
 
