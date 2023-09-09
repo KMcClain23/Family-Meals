5b1c03ff5419ea4d76616246502567f686a44a49
snoopyods <snoopyods@gmail.com>
2023-09-09 15:54:10 -0700

    Token auth and error handling for user already exist, done.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Auth } from './pages/Auth';
+import { CreateRecipes } from './pages/CreateRecipe';
+import { SavedRecipes } from './pages/SavedRecipes';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Auth' element={<Auth />} />
+          <Route path='/CreateRecipes' element={<CreateRecipes />} />
+          <Route path='/SavedRecipes' element={<SavedRecipes />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/CreateRecipe.js
@@ -0,0 +1,3 @@
+export const CreateRecipes = () => {
+    return <div>CreateRecipes</div>;
+}
