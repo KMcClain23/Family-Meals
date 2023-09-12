f753a71c385d6492b70b0d0bae3ccbde8b5f3821
snoopyods <snoopyods@gmail.com>
2023-09-12 16:23:51 -0700

    Prettied up the Navbar again

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,85 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+import { Menu } from 'antd';
+import { HomeOutlined, LoginOutlined, LogoutOutlined, PlusCircleOutlined, HeartOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
 
 export const Navbar = () => {
-    const [cookies, setCookies] = useCookies(["access_token"]);
-    const navigate = useNavigate();
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
 
-    const logout = () => {
-        setCookies("access_token", "");
-        window.localStorage.clear();
-        navigate("/Auth");
-    };
-    return (
-        <div className="Navbar">
-            <Link to="/">Home</Link>
-        {!cookies.access_token ? (
-            <Link to="/Login">Login/Register</Link>
-            ) : (
-            <>
-            <Link to="/CreateRecipe">Create Recipe</Link>
-            <Link to="/SavedRecipes">Saved Recipes</Link>
-            <button onClick={logout}> Logout </button>
-            </>
-        )}
-        </div>
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.clear();
+    navigate("/Auth");
+  };
+
+  const items = [
+    {
+      label: 'Home',
+      key: 'home',
+      icon: <HomeOutlined />,
+      path: "/"
+    },
+  ];
+
+  if (cookies.access_token) {
+    items.push(
+      {
+        label: 'Recipes',
+        key: 'recipes',
+        icon: <HeartOutlined />,
+        children: [
+          {
+            label: 'Create Recipe',
+            key: 'create',
+            icon: <PlusCircleOutlined />,
+            path: "/create"
+          },
+          {
+            label: 'Saved Recipes',
+            key: 'saved',
+            icon: <HeartOutlined />,
+            path: "/saved"
+          },
+        ]
+      },
+      {
+        label: 'Logout',
+        key: 'logout',
+        icon: <LogoutOutlined />,
+        path: "/logout",
+        onClick: logout
+      }
     );
-    };
+  } else {
+    items.push({
+      label: 'Login/Register',
+      key: 'auth',
+      icon: <LoginOutlined />,
+      path: "/login"
+    });
+  }
+
+  return (
+    <Menu mode="horizontal" theme="dark">
+      {items.map(item => (
+        item.children ? (
+          <SubMenu key={item.key} icon={item.icon} title={item.label}>
+            {item.children.map(subItem => (
+              <Menu.Item key={subItem.key} icon={subItem.icon} onClick={() => navigate(subItem.path)}>
+                {subItem.label}
+              </Menu.Item>
+            ))}
+          </SubMenu>
+        ) : (
+          <Menu.Item key={item.key} icon={item.icon} onClick={item.onClick || (() => navigate(item.path))}>
+            {item.label}
+          </Menu.Item>
+        )
+      ))}
+    </Menu>
+  );
+};
